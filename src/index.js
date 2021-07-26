48f16de1602dfe170e607b468c2b48aca03045b6
Kemovf <cdutlzc@gmail.com>
2021-07-26 10:05:13 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 //React项目入口文件
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './Page';
 // import App from './App copy';
-import App from './DashBorad';
+// import App from './DashBorad';
 import { AppProvider } from './store';
-// import './style/index.less';
+import 'antd/dist/antd.css';
+
 
 
 ReactDOM.render(<AppProvider>
